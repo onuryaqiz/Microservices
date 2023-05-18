dcfe3c30b299db43eb6bb9a1fadcc0a8e6cdbc4d
onuryaqiz <onur.yaqiz@outlook.com>
2023-05-18 17:15:51 +0300

    ResponseDto ve Static Factory Method oluşturuldu.

---
+++ Shared/FreeCourse.Shared/Dtos/NoContent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Shared.Dtos
+{
+    public class NoContent //Response'da bir şey dönülmeyecekse Response<T>'de nocontent döneriz.
+    {
+    }
+}
---
+++ Shared/FreeCourse.Shared/Dtos/ResponseDto.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Shared.Dtos
+{
+    public class ResponseDto<T>
+    {
+        public T Data { get; private set; }
+
+        [JsonIgnore]
+        public int StatusCode { get; private set; } //Postman'den istek atıldığında response code görüyoruz fakat bu kodu tutabilmek için de 2 kere almamak için JsonIgnore yaptık. Ama kaydedilecek de sisteme.
+
+        [JsonIgnore]
+        public bool IsSuccessful { get; private set; }
+
+        public List<string> Errors { get; set; }
+
+        //Static factory method
+        public static ResponseDto<T> Success(T data, int statusCode)
+
+        {
+            return new ResponseDto<T> { Data = data, StatusCode = statusCode, IsSuccessful = true };
+        }
+
+        public static ResponseDto<T> Success(int statusCode)
+        {
+            return new ResponseDto<T> { Data = default(T), StatusCode = statusCode, IsSuccessful = true };
+        }
+
+        public static ResponseDto<T> Fail(List<string> errors, int statusCode)
+        {
+            return new ResponseDto<T>
+            {
+                Errors = errors,
+                StatusCode = statusCode,
+                IsSuccessful = false
+            };
+        }
+
+        public static ResponseDto<T> Fail(string error, int statusCode)
+        {
+            return new ResponseDto<T> { Errors = new List<string>() { error }, StatusCode = statusCode, IsSuccessful = false };
+        }
+    }
+}
