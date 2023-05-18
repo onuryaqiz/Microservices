4f39ec5b1ebb18e49e7eca2d6cdc3322186541e9
onuryaqiz <onur.yaqiz@outlook.com>
2023-05-18 19:12:29 +0300

    DatabaseSetting ayarları , option pattern üzerinden kullandık. Ve config ayarları yapıldı.

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Settings/IDatabaseSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace FreeCourse.Services.Catalog.Settings
+{
+    public interface IDatabaseSettings //Config ayarlarını bir sınıf üzerinden okumak için options pattern kullanıyoruz. O yüzden interface oluşturduk.
+    {
+        public string CourseCollectionName { get; set; }
+
+        public string CategoryCollectionName { get; set; }
+
+        public string ConnectionString { get; set; }
+
+        public string DatabaseName { get; set; }
+    }
+}
