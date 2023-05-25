84146dd7a5e8138c08c5aac0d7f6ad0794f34306
onuryaqiz <onur.yaqiz@outlook.com>
2023-05-25 17:00:35 +0300

    CoursesController eklendi. CustomBaseController oluşturuldu . Ve generic hale getirildi. içindeki metodlar yazıldı

---
+++ Shared/FreeCourse.Shared/ControllerBases/CustomBaseController.cs
@@ -0,0 +1,21 @@
+﻿using FreeCourse.Shared.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Shared.ControllerBases
+{
+    public class CustomBaseController:ControllerBase
+    {
+        public IActionResult CreateActionResultInstance<T>(Response<T> response)
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode, //CoursesController'daki response statusCode her seferinde çağırmak yerine generic olarak burada yazdık.
+            };
+        }
+    }
+}
