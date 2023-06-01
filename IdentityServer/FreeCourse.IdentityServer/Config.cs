fa0cab46e10bad69c710d5272ddc7849b6b0a317
onuryaqiz <onur.yaqiz@outlook.com>
2023-06-01 22:56:39 +0300

    ...

--- IdentityServer/FreeCourse.IdentityServer/Config.cs
+++ IdentityServer/FreeCourse.IdentityServer/Config.cs
@@ -52,6 +52,7 @@ namespace FreeCourse.IdentityServer
                 {
                     ClientName="Asp.Net Core MVC",
                     ClientId="WebMvcClientForUser",
+                    AllowOfflineAccess=true,
                     ClientSecrets={new Secret("secret".Sha256())},
                     AllowedGrantTypes=GrantTypes.ResourceOwnerPassword,//GrantTypes=Akış Tipi,ResourceOwnerPasswordAndClientCredentials'da Refresh token yoktur.
                     AllowedScopes={ IdentityServerConstants.StandardScopes.Email,IdentityServerConstants.StandardScopes.OpenId,IdentityServerConstants.StandardScopes.Profile,IdentityServerConstants.StandardScopes.OfflineAccess,"roles"},
