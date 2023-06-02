cab2c95e4f43ee8704575b0b0fdde4862d562f30
onuryaqiz <onur.yaqiz@outlook.com>
2023-06-02 23:52:59 +0300

    appsettings'e identityserver Url ve program.cs gerekli middleware eklendi.

--- Services/PhotoStock/FreeCourse.Services.PhotoStock/Program.cs
+++ Services/PhotoStock/FreeCourse.Services.PhotoStock/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+options.Authority = builder.Configuration["IdentityServerURL"];
+options.Audience = "resource_catalog";
+options.RequireHttpsMetadata = false;
+
+});
 
 var app = builder.Build();
 
