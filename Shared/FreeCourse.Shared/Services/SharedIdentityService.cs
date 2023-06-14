df896efa730bf9d79a88d0b1ac8325d08dfd9f33
onuryaqiz <onur.yaqiz@outlook.com>
2023-06-14 17:30:29 +0300

    IdentityService

---
+++ Shared/FreeCourse.Shared/Services/SharedIdentityService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreeCourse.Shared.Services
+{
+    public class SharedIdentityService : ISharedIdentityService
+    {
+        private IHttpContextAccessor
+        public string GetUserId => throw new NotImplementedException();
+    }
+}
