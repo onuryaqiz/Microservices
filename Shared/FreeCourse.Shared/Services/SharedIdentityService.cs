61a1ac3cdd05a24002eb98d0bf5f6c5ae6a7e8c3
onuryaqiz <onur.yaqiz@outlook.com>
2023-06-14 17:31:01 +0300

    ..

--- Shared/FreeCourse.Shared/Services/SharedIdentityService.cs
+++ Shared/FreeCourse.Shared/Services/SharedIdentityService.cs
@@ -1,13 +1,20 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace FreeCourse.Shared.Services
 {
     public class SharedIdentityService : ISharedIdentityService
     {
-        private IHttpContextAccessor
-        public string GetUserId => throw new NotImplementedException();
+        private IHttpContextAccessor _httpContextAccessor;
+
+        public SharedIdentityService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string GetUserId => _httpContextAccessor.HttpContext.User.FindFirst("sub").Value; //Type sub'a denk geliyor.
     }
 }
