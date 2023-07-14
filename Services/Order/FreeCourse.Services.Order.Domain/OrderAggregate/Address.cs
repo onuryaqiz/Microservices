0382b4c27a8a7637c9f435f78d1c95e2d0caba1e
onuryaqiz <onur.yaqiz@outlook.com>
2023-07-14 16:36:11 +0300

    Address , OrderItem

---
+++ Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/Address.cs
@@ -0,0 +1,44 @@
+﻿using FreeCourse.Services.Order.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Order.Domain.OrderAggregate
+{
+    public class Address : ValueObject
+    {
+        public string Province { get; private set; }
+
+        public string District { get; private set; }
+
+        public string Street { get; private set; }
+
+        public string ZipCode { get; private set; }
+
+        public string Line { get; private set; }
+
+        public Address(string province, string district, string street, string zipCode, string line) //set'leri private yaptığımız için kendi constructor'ımızı yaratıp buradan nesne oluşturabileceğiz.
+        {
+            Province = province;
+            District = district;
+            Street = street;
+            ZipCode = zipCode;
+            Line = line;
+        }
+//        public void SetZipCode(string zipCode)
+//        {
+//        //business code
+//            ZipCode=zipCode;
+//        }
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Province;
+            yield return District; 
+            yield return Street; 
+            yield return ZipCode; 
+            yield return Line;
+        }
+    }
+}
