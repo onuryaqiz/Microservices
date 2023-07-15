4caf46c3530f00ffa40b9bc0d25118a0500742d2
onuryaqiz <onur.yaqiz@outlook.com>
2023-07-15 20:11:01 +0300

    IdentityServer ayarları Order API'ye entegre edildi.

--- Services/Order/FreeCourse.Services.Order.API/Program.cs
+++ Services/Order/FreeCourse.Services.Order.API/Program.cs
@@ -1,16 +1,20 @@
-using FreeCourse.Services.Order.Infrastructure;
+﻿using FreeCourse.Services.Order.Infrastructure;
 using FreeCourse.Shared.Services;
 using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
+using System.IdentityModel.Tokens.Jwt;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<OrderDbContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), configure =>
@@ -19,9 +23,27 @@ builder.Services.AddDbContext<OrderDbContext>(opt =>
 
     });
 });
+
 builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.Authority = builder.Configuration["IdentityServerURL"];
+    options.Audience = "resource_order";
+    options.RequireHttpsMetadata = false;
+
+
+});
+
+var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build(); //Controller tarafında koruma altına aldık. Request header'da mutlaka token olmak zorunda.
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");//nameidentifier'a map'leme.Claims'lerde direkt sub'ı gösterecek demektir.
+builder.Services.AddControllers(opt =>
+{
+    opt.Filters.Add(new AuthorizeFilter(requireAuthorizePolicy));
+});
+
 builder.Services.AddScoped<ISharedIdentityService,SharedIdentityService>();
-builder.Services.AddMediatR(typeof(Application.Handlers.CreateOrderCommandHandler).Assembly);
+builder.Services.AddMediatR(typeof(FreeCourse.Services.Order.Application.Handlers.CreateOrderCommandHandler).Assembly);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -30,7 +52,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
