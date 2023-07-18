0d2274ffd9e2dbb55b8764a520316e551f089e40
onuryaqiz <onur.yaqiz@outlook.com>
2023-07-18 16:31:12 +0300

    ...

--- FreeCourse.Gateway/Program.cs
+++ FreeCourse.Gateway/Program.cs
@@ -1,10 +1,14 @@
-using Ocelot.DependencyInjection;
+﻿using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 builder.Services.AddOcelot();
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config.AddJsonFile($"configuration.{hostingContext.HostingEnvironment.EnvironmentName.ToLower()}.json").AddEnvironmentVariables(); //TODO: Configuration kullanılabilir mi? Araştıralacak!
+});
 
 app.MapGet("/", () => "Hello World!");
 
