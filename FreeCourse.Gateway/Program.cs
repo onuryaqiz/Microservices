ca9261af0cafcee7a0a72d90b823933f56a1dae6
onuryaqiz <onur.yaqiz@outlook.com>
2023-07-18 16:17:30 +0300

    Ocelot eklendi

---
+++ FreeCourse.Gateway/Program.cs
@@ -0,0 +1,13 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+builder.Services.AddOcelot();
+
+app.MapGet("/", () => "Hello World!");
+
+await app.UseOcelot();
+
+app.Run();
