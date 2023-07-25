4d87d5de868e591f5b78d51c2a7aa5537e876f0d
onuryaqiz <onur.yaqiz@outlook.com>
2023-07-26 00:41:37 +0300

    Login Page

---
+++ Frontends/FreeCourse.Web/Controllers/AutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Web.Controllers
+{
+    public class AutController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
